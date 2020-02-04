b71194c8eea9b1ba2f446bb8ba5db2041e398ae9
Efremova Ekaterina <kayfremava@yandex.by>
2020-02-04 23:32:04 +0300

    budget

--- budget/script/script.js
+++ budget/script/script.js
@@ -8,31 +8,28 @@ let isNumber = function(n) {
 function guessTheNumber(){
   let guessednumber = Math.ceil(Math.random()*100);// от 1 до 100
   console.log(guessednumber);
-  let customnumber = prompt('Угадай число от 1 до 100');
-  if (customnumber === null) {
-    return(alert('Игре конец :('));
-  }
   function guessTheNumber02(){
+    let customnumber = prompt('Угадай число от 1 до 100');
     if (customnumber === null) {
-      return(alert('Игре конец :('));
-    }  
-
-    else if (!isNumber(customnumber)){
-      customnumber = prompt('Введи число');
-    } 
-      
-    else if (customnumber > guessednumber) {
-      customnumber = prompt('Загаданное число меньше, введи заново');
-    } 
-      
-    else if (customnumber < guessednumber) {
-      customnumber = prompt('Загаданное число больше, введи заново');
-    } 
-    
-    else if (customnumber == guessednumber){
-      return(alert('Ты угадал число ' + guessednumber));
+      alert('Игре конец');
     }
-    guessTheNumber02();
+    while(guessednumber != customnumber){
+      if (!isNumber(customnumber)){
+        customnumber = prompt('Введи число');
+      } 
+      
+      else if (customnumber > guessednumber) {
+        customnumber = prompt('Загаданное число меньше, введи заново');
+      } 
+      
+      else if (customnumber < guessednumber) {
+        customnumber = prompt('Загаданное число больше, введи заново');
+      } 
+      else if (customnumber === null) {
+        alert('Игре конец');
+      }
+    }
+    alert('Ты угадал число ' + guessednumber);
   }
   guessTheNumber02();
 }
