44fe2d25ed2e25cacab52d20db00661d6e86028e
Efremova Ekaterina <kayfremava@yandex.by>
2020-02-22 23:12:58 +0300

    lesson13

---
+++ lesson13/script/main.js
@@ -0,0 +1,7 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  let form = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('#todo'),
+    completedList = document.querySelector('#completed');
+});
